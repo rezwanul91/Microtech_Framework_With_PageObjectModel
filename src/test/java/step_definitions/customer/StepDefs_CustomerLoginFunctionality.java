9665e184a8d0214b8b79f72eeba95517a43e33c6
rezwanul91 <mohammadhaquer@gmail.com>
2023-06-21 12:49:03 -0400

    update project

--- src/test/java/step_definitions/customer/StepDefs_CustomerLoginFunctionality.java
+++ src/test/java/step_definitions/customer/StepDefs_CustomerLoginFunctionality.java
@@ -46,6 +46,8 @@ public class StepDefs_CustomerLoginFunctionality extends Base{
 	public void verify_that_I_am_in_CUSTOMER_Home_Page() {
 		String welcomeMsg =  getElementText(By.xpath("//h2[2]"));
 		assertEquals("Welcome David" , welcomeMsg);
+		System.out.println("Verification done");
+
 	}
 
 }
